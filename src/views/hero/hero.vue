9f57c40a724dbc4aa97578a7c0c36c676f892383
532466416 <532466416@qq.com>
2019-07-08 19:52:51 +0800

    8：英雄列表请求数据并渲染

--- src/views/hero/hero.vue
+++ src/views/hero/hero.vue
@@ -1,44 +1,64 @@
 <template>
-    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h2 class="sub-header">英雄管理</h2>
-          <a class="btn btn-success" href="add.html">Add</a>
-          <div class="table-responsive">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>姓名</th>
-                  <th>性别</th>
-                  <th>操作</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                  <td>
-                    <a href="edit.html">edit</a>
-                    &nbsp;&nbsp;
-                    <a
-                      href="javascript:window.confirm('Are you sure?')"
-                    >delete</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+    <h2 class="sub-header">英雄管理</h2>
+    <a class="btn btn-success" href="add.html">Add</a>
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>姓名</th>
+            <th>性别</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item,index) in list" :key="index">
+            <td>{{item.id}}</td>
+            <td>{{item.name}}</td>
+            <td>{{item.gender}}</td>
+            <td>
+              <a href="edit.html">edit</a>
+              &nbsp;&nbsp;
+              <a href="javascript:window.confirm('Are you sure?')">delete</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from "axios";
+// 为什么不能把它放在router.js或者main.js
 
-}
+export default {
+  data() {
+    return {
+      list: []
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      axios.get("http://localhost:3000/users").then(respons => {
+        const { status, data } = respons;
+        if (status == 200) {
+          this.list = data;
+        } else {
+          alert("服务获取失败");
+        }
+      }).catch(err=>{
+        alert('服务器异常');
+        console.log(err)
+      })
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
